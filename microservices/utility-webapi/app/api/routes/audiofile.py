4df26ba32e066b7a9f9e88fe457c47dbeb79391e
koki622 <koki08018907926@gmail.com>
2024-09-11 12:08:55 +0900

    オーディオファイルディレクトリを削除するwebapiエンドポイントを追加

--- microservices/utility-webapi/app/api/routes/audiofile.py
+++ microservices/utility-webapi/app/api/routes/audiofile.py
@@ -3,7 +3,7 @@ import shutil
 import shortuuid
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, Request
 from app.core.config import settings
-from app.api.deps import get_consumer_dir
+from app.api.deps import get_consumer_dir, get_audiofile_path
 from pydub import AudioSegment
 
 router = APIRouter()
@@ -28,8 +28,14 @@ async def save_audiofile(request: Request, audiofile: UploadFile = File(...), co
         shutil.copyfileobj(audiofile.file, buffer)
 
     if audiofile.content_type == 'audio/mpeg':
+        # アップロードされた音声ファイルがmp3の場合、wavに変換
         mp3_audio = AudioSegment.from_mp3(audiofile_path)
         wav_audiofile_path = audiofile_dir / (rand_dirname + '.wav')
         mp3_audio.export(wav_audiofile_path, format='wav')
 
     return {'filename': audiofile.filename, 'dir_name': rand_dirname}
+
+@router.delete("/", description='オーディオファイルを削除する。')
+async def delete_audiofile(audiofile_path: Path = Depends(get_audiofile_path)):
+    shutil.rmtree(audiofile_path.parent)
+    return('ok')
