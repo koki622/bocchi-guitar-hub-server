a439e84a985fda6cde53c0d154cf99e2eb2d2ac5
koki622 <koki08018907926@gmail.com>
2024-09-23 22:07:22 +0900

    heavy_job.pyでジョブの結果の通知先が固定されていたので修正。コメントの追加。

--- microservices/utility-webapi/app/core/heavy_job.py
+++ microservices/utility-webapi/app/core/heavy_job.py
@@ -1,3 +1,10 @@
+"""ジョブの管理と、その状況の通知を扱うモジュール。
+
+このモジュールは、Python RQを用いたジョブの管理と、ジョブの状態や結果をリアルタイムで通知するための機能を提供します。
+ジョブは、宛先APIへのリクエストで構成され、ジョブの実行は、宛先のAPIへリクエストが送信することを指します。
+サーバーサイドで実行されるジョブのキューイング、ジョブの状況と結果の通知を非同期に行います。
+
+"""
 import asyncio
 from redis import Redis
 import redis.asyncio
@@ -15,6 +22,17 @@ from datetime import datetime, timezone
 from collections.abc import AsyncGenerator
 
 async def get_job_result(job: Job, sleep_time: Union[int, float]) -> any:
+    """ジョブの結果を待つ。
+
+    ジョブの結果をsleep_time間隔で取得する。
+
+    Args:
+        job (Job): Jobのインスタンス。
+        sleep_time (Union[int, float]): どのくらいの頻度で結果を確認するか。
+
+    Returns:
+        any: Jobの結果。
+    """
     while True:
         await asyncio.sleep(sleep_time)
         job_result = job.latest_result()
@@ -27,7 +45,7 @@ async def get_job_result(job: Job, sleep_time: Union[int, float]) -> any:
         print('Job Result is None')
     return job_result
 
-def datetime_to_stream_id(dt: datetime, sequence: Union[int, None] = 0) -> str:
+def _datetime_to_stream_id(dt: datetime, sequence: Union[int, None] = 0) -> str:
     """datetimeをRedis StreamsのStreamId形式に変換する
 
     Args:
@@ -42,27 +60,53 @@ def datetime_to_stream_id(dt: datetime, sequence: Union[int, None] = 0) -> str:
         return str(timestamp_ms)
     return f"{timestamp_ms}-{sequence}"
 
-def notify_job_success(job: Job, connection, result, *args, **kwargs):
-    """ジョブが成功したことをRedis Streamsで通知する。
+def _queue_name_to_stream_name(queue_name: str) -> str:
+    return f'{queue_name}_notify_stream'
+
+def _notify_job(job: Job, connection, send_data: dict):
+    """ジョブの結果をRedis Streamsを用いて通知する。
+
+    Args:
+        job (Job): Jobのインスタンス。
+        connection (_type_): Redisのコネクション。
+        send_data (dict): Redis Streamsで送信するデータ。
+    """
+    # ストリームIDを現在時刻から生成する
+    stream_id = _datetime_to_stream_id(datetime.now(timezone.utc), None)
+
+    # 結果の通知先ストリームチャンネル名をキュー名から取得する
+    notify_stream_name = _queue_name_to_stream_name(job.meta.get('queue_name'))
+
+    s_id = connection.xadd(notify_stream_name, send_data, id=stream_id)
+    print('メッセージを送信しました', 'sid:', s_id)
+
+def _notify_job_success(job: Job, connection, result, *args, **kwargs):
+    """ジョブが成功したことを通知する。
 
     rqでジョブをキューに入れる際に、ジョブ成功後に呼び出されるコールバック関数として用いる。
 
     Args:
-        job (Job): _description_
-        connection (_type_): _description_
+        job (Job): jobのインスタンス。
+        connection (_type_): Redisのコネクション。
         result (_type_): ジョブの結果。
     """
-    stream_id = datetime_to_stream_id(datetime.now(timezone.utc), None)
-    print(stream_id)
-    #meta = job.meta.get('queue_name')
-    print(job.origin)
-    s_id = connection.xadd('gpu_stream', {'message':f"{result}:{job.id}"}, id=stream_id)
-    print('メッセージを送信しました', 'sid:', s_id)
+    send_data = {'message':f"{result}:{job.id}"}
+    _notify_job(job, connection, send_data)
 
-def notify_job_failure(job: Job, connection, type, value, traceback):
-    stream_id = datetime_to_stream_id(datetime.now(timezone.utc), None)
-    s_id = connection.xadd('gpu_stream', {'message':f"{type}:{job.id}"}, id=stream_id)
-    print('メッセージを送信しました', 'sid:', s_id)
+def _notify_job_failure(job: Job, connection, type, value, traceback):
+    """ジョブが失敗したことを通知する。
+
+    rqでジョブをキューに入れる際に、ジョブ成功後に呼び出されるコールバック関数として用いる。
+    
+    Args:
+        job (Job): jobのインスタンス。
+        connection (_type_): Redisのコネクション。
+        type (_type_): _description_
+        value (_type_): _description_
+        traceback (_type_): _description_
+    """
+    send_data = {'message':f"{type}:{job.id}"}
+    _notify_job(job, connection, send_data)
 
 async def route_job(
     dst_api_host: str,
@@ -76,6 +120,7 @@ async def route_job(
     url = f"http://{dst_api_host}:{dst_api_port}{path}"
     try:
         response = requests.post(url=url, json=payload, headers=headers, timeout=(connect_timeout, read_timeout))
+        response.raise_for_status()
         return 'ok'
     except Timeout as e:
         raise e
@@ -107,7 +152,7 @@ class HeavyJob:
             queue_position: Union[int, None], 
             enqueued_at:datetime
     ) -> AsyncGenerator[int, None]:
-        last_stream_id = datetime_to_stream_id(enqueued_at)
+        last_stream_id = _datetime_to_stream_id(enqueued_at)
         is_finished: bool= False
         while True:
             messages = await self.redis_asyncio_conn.xread(streams={stream_name: last_stream_id})
@@ -147,8 +192,26 @@ class HeavyJob:
         request_body: dict = None, 
         request_read_timeout: Union[int, float] = None
     ):
+        """ジョブキューの状況をServer Sent Eventsでレスポンスするためのジェネレータを返します。
+
+        Args:
+            request (Request): _description_
+            queue_name (str): _description_
+            job_timeout (Union[int, str], optional): _description_. Defaults to 60.
+            request_path (str, optional): _description_. Defaults to "/".
+            request_headers (dict, optional): _description_. Defaults to None.
+            request_body (dict, optional): _description_. Defaults to None.
+            request_read_timeout (Union[int, float], optional): _description_. Defaults to None.
+
+        Raises:
+            e: _description_
+
+        Yields:
+            _type_: _description_
+        """
         redis_conn = Redis(self.redis_host, self.redis_port)
         q = Queue(name=queue_name, connection=redis_conn)
+    
         job_id = shortuuid.ShortUUID().random(length=10)
         job_kwargs = {
             "dst_api_host": self.dst_api_host,
@@ -167,23 +230,32 @@ class HeavyJob:
                 job_timeout=job_timeout,
                 meta={'queue_name':queue_name},
                 kwargs=job_kwargs,
-                on_success=Callback(notify_job_success),
-                on_failure=Callback(notify_job_failure)
+                on_success=Callback(_notify_job_success),
+                on_failure=Callback(_notify_job_failure)
             )
+
             job_id = job_queue.id
             enqueued_at = job_queue.enqueued_at
-            print(datetime_to_stream_id(enqueued_at))
+            
             yield self.generate_job_status_message(job_id, 'enqueue success')
     
             queue_position = job_queue.get_position()
+        
+            notify_stream_name = _queue_name_to_stream_name(queue_name)
+
             if queue_position is None:
+                # queue_positionが空の状態は、既に処理中であることを示す。
                 yield self.generate_job_status_message(job_id, 'processing soon')
-            async for queue_position in self.response_queue_status_from_stream(request, 'gpu_stream', job_id, queue_position, enqueued_at):
+
+            # queue_positionの初期値から現在のキューの位置を推定し、位置に変化がある度に通知する。
+            async for queue_position in self.response_queue_status_from_stream(request, notify_stream_name, job_id, queue_position, enqueued_at):
                 if queue_position < 0:
+                    # 0未満はキューを抜け出して、処理が始まることを示す。
                     yield self.generate_job_status_message(job_id, 'processing soon')
                 else:
                     yield self.generate_job_status_message(job_id, 'queued', queue_position)
 
+            # すぐに結果が反映されないので、0.1秒待ってから結果を取得
             job_result = await get_job_result(job_queue, 0.1)
             yield{'event':'result','data': job_result}
 
